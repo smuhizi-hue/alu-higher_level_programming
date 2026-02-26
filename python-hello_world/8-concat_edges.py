28e93edc03114fd6d5af3dd7172b0098645e0322
Smuhizi Hue <smuhizi@example.com>
2026-02-26 01:05:32 -0800

    ohj

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted. Interactive, object-oriented programming\
+        languange that combiness remarkable power with very syntax"
+print(str[39:66],end='')
+print("with", str[:6])
