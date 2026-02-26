2b3dee0d5b03500d5c30d12879d8db156c6634fd
Smuhizi Hue <smuhizi@example.com>
2026-02-26 01:29:51 -0800

    onj

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted. Interactive, object-oriented programming\
-        languange that combiness remarkable power with very syntax"
-print(str[39:66],end='')
-print("with", str[:6])
+str1 = "object-oriented programming"
+str2 = "with Python"
+print(str1 + str2[4:])
