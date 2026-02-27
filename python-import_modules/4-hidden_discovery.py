7f191af715602368977569b8d0a7cb4f1ecc6b68
Smuhizi Hue <smuhizi@example.com>
2026-02-27 01:19:07 -0800

    poij

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         code_obj =marshal.load(f) # read the code object
 
     # get all names in thecode object that do not start with _
-    names = [name for name in code_obj.co_names if not name.startswith('_')])
+    names = [name for name in code_obj.co_names if not name.startswith('_')]
 
     # Print names in alphabetical order
     for name in sorted(names):
