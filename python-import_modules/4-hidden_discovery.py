296d7f5181253010d0cac2547251800a8ad6beeb
Smuhizi Hue <smuhizi@example.com>
2026-02-27 01:16:01 -0800

    pjk

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@ import marshal
 
 if __name__ == "__main__":
     with open("hidden_4.pyc", "rb") as f:  
-    f.read(16) #skip header (Python 3.8)
-    code_obj =marshal.load(f) # read the code object
+        f.read(16) #skip header (Python 3.8)
+        code_obj =marshal.load(f) # read the code object
 
     # get all names in thecode object that do not start with _
     names = [name for name in code_obj.co_names if not name.startswith('_')])
