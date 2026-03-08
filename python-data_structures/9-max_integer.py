6bd9f629e533fbc33a969ca74b6644b42c2aebb4
Smuhizi Hue <smuhizi@example.com>
2026-03-08 01:29:53 -0800

    mkbh

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    for n in my_list:
+        if n > max_val:
+            max_val = n
+    return max_val
