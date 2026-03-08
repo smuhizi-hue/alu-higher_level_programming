58cf61fb20815fea35c416a2318db60ee9ab9a01
Smuhizi Hue <smuhizi@example.com>
2026-03-08 01:13:21 -0800

    omon

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/urs/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            element = row[i]
+            print("{:d}".format(element), end="" if i == len(row) - 1 else " ")
+        print()
