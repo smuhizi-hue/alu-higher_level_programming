22cb9606259ba03167bb5d285267e8675dab7700
Smuhizi Hue <smuhizi@example.com>
2026-03-10 03:47:17 -0700

    kjhyv

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythom3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
     for element in my_list:
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
             new_list.append(replace)
         else:
             new_list.append(element)
-    return new_list
+    return new_lit
