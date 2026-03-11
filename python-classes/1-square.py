bef6e9c8404d890675915b42418ab6cd7acc5825
Smuhizi Hue <smuhizi@example.com>
2026-03-11 08:41:50 -0700

    inimo

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a square class."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize square with size."""
