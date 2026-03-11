7854d0e55756b73d952d3ac0c330f6743c3a6f34
Smuhizi Hue <smuhizi@example.com>
2026-03-11 08:50:44 -0700

    on

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with size validation."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
