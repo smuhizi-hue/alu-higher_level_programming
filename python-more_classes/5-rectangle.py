dbf95d1b595820ae4a535b63a0468595da7698fd
Smuhizi Hue <smuhizi@example.com>
2026-03-16 04:32:19 -0700

    oimop

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the rectangle printed with #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rows = []
+        for _ in range(self.__height):
+            rows.append("#" * self.__width)
+        return "\n".join(rows)
+
+    def __repr__(self):
+        """Return a string representation for eval()"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print message when rectangle instance is deleted"""
+        print("Bye rectangle...")
