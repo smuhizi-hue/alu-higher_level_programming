259a71a3d971c13f3a605faa7c30503031df10b5
Smuhizi Hue <smuhizi@example.com>
2026-04-03 05:51:33 -0700

    poi

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class.
+"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: The dictionary representation of the instance.
+        """
+        return self.__dict__
