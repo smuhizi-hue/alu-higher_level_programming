ed1d785e50bca2a086a877255c968cd861ea9300
Smuhizi Hue <smuhizi@example.com>
2026-04-03 06:34:39 -0700

    poi

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """A class with a public instance method area"""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
