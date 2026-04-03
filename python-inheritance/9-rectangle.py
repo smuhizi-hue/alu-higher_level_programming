d10232adcc1ca85ac9a989e75570ccbc9e2c389f
Smuhizi Hue <smuhizi@example.com>
2026-04-03 06:50:33 -0700

    pooi

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for Rectangle class that inherits from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initializes width and height after validation"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the print() and str() representation of the Rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
