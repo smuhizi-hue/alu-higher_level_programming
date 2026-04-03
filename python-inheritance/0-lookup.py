8e0be868bba7f19c5347f784f883e2dbfc07d5d6
Smuhizi Hue <smuhizi@example.com>
2026-04-03 06:18:48 -0700

    poi

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,6 @@ Module for lookup function
 """
 
 
-    def lookup(obj):
-        """Return the list of available attributes and methods of an object."""
-        return dir(obj)
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)
